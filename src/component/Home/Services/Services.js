641d0f64bd2a63adeb8e6866fff0999bc51be5c9
Siddika-akhter <siddika.tas28@gmail.com>
2020-11-15 12:03:51 +0600

    first commit

---
+++ src/component/Home/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <section>
+            <h2>this is service section</h2>
+        </section>
+    );
+};
+
+export default Services;
